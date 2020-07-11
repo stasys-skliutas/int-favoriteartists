dbffe2abe5598e90e7484b52a48933796adb0057
Stasys Skliutas <stasys.skliutas@gmail.com>
2020-07-11 19:35:27 +0300

    Add initial project setup

---
+++ src/main/java/dev/stasys/favoriteartists/FavoriteartistsApplication.java
@@ -0,0 +1,13 @@
+package dev.stasys.favoriteartists;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoriteartistsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoriteartistsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/stasys/favoriteartists/FavoriteartistsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.stasys.favoriteartists;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoriteartistsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
